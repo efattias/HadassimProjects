f76d0cb625e6be91ab02b272184b6c0305d4d372
efattias <efattias@g.jct.ac.il>
2025-04-01 16:57:24 +0300

    basic

--- PartA/n_most_frequent_logs.py
+++ PartA/n_most_frequent_logs.py
@@ -1,3 +1,14 @@
+from collections import Counter
+
 with open(r"PartA\logs.txt",'r',encoding='utf-8') as f:
-    print(f.readline());
-    print(f.readline());
+    chunk_size = 10000
+    f_chunk = f.read(chunk_size)
+    c = Counter()
+    
+    while True:
+        chunk = [f.readline().strip() for _ in range(chunk_size)]
+        if not chunk or all(line == "" for line in chunk):  # בדיקה אם הגענו לסוף הקובץ
+            break
+        c.update(chunk)
+    print(c.most_common(10))
+    
